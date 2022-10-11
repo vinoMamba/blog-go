5a88ca8c05bef019c4aae24657f808dcd335805f
vino <vino0908@outlook.com>
2022-10-11 23:19:42 +0800

    add content-type

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Hello, 这里是 goblo</h1>")
 		fmt.Fprint(w, "请求的路径为:"+r.URL.Path)
