9d175e258a4e81d2ca548d49be0c26b02f53ab41
vino <vino0908@outlook.com>
2022-10-11 23:16:54 +0800

    add air reload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vinoMamba/goblog
+
+go 1.18
--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+		fmt.Fprint(w, "<h1>Hello, 这里是 goblo</h1>")
 		fmt.Fprint(w, "请求的路径为:"+r.URL.Path)
 	} else if r.URL.Path == "/about" {
 		fmt.Fprint(w, "<h1>Hello, 这里是关于页面</h1>")
