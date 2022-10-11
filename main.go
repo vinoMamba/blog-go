cb098b2e50ad7b36bd8087d0c00c5d6b395f5c6d
vino <vino0908@outlook.com>
2022-10-11 23:57:02 +0800

    拆分路由

--- main.go
+++ main.go
@@ -5,20 +5,14 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Hello, 这里是 goblo</h1>")
-		fmt.Fprint(w, "请求的路径为:"+r.URL.Path)
-	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "<h1>Hello, 这里是关于页面</h1>")
-		fmt.Fprint(w, "请求的路径为:"+r.URL.Path)
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "404")
-	}
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "About Page")
 }
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", defaultHandler)
+	http.HandleFunc("/about", aboutHandler)
 	http.ListenAndServe(":3000", nil)
 }
