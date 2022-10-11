8bcc15a101f1828def232bcc0364e68082e659e2
vino <vino0908@outlook.com>
2022-10-11 22:57:01 +0800

    添加多个页面

--- main.go
+++ main.go
@@ -6,7 +6,15 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+		fmt.Fprint(w, "请求的路径为:"+r.URL.Path)
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "<h1>Hello, 这里是关于页面</h1>")
+		fmt.Fprint(w, "请求的路径为:"+r.URL.Path)
+	} else {
+		fmt.Fprint(w, "404")
+	}
 }
 func main() {
 	http.HandleFunc("/", handlerFunc)
