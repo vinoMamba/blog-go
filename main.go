aa6f5856827b46f50452725278f8d68c5af05d79
vino <vino0908@outlook.com>
2022-10-12 13:18:46 +0800

    feat: 表单验证

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+	"net/url"
 	"strings"
 
 	"github.com/gorilla/mux"
 )
+
+var router = mux.NewRouter()
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
@@ -17,6 +21,7 @@ func aboutHandler(w http.ResponseWriter, r *http.Request) {
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>404 页面未找到</h1>")
 }
+
 func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	fmt.Printf("vars: %#v", vars)
@@ -24,12 +29,96 @@ func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>文章</h1>")
 	fmt.Fprint(w, id)
 }
+
 func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>文章列表</h1>")
 }
+
+type ArticlesFormData struct {
+	Title  string
+	Body   string
+	URL    *url.URL
+	Errors map[string]string
+}
+
+func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.PostFormValue("title")
+	body := r.PostFormValue("body")
+	errors := make(map[string]string)
+	if title == "" {
+		errors["title"] = "标题不可为空"
+	} else if len(title) < 3 || len(title) > 40 {
+		errors["title"] = "标题长度需介于 3-40"
+	}
+	if body == "" {
+		errors["body"] = "内容不能为空"
+	} else if len(body) < 10 {
+		errors["body"] = "内容长度需大于或等于 10 个字节"
+	}
+	if len(errors) == 0 {
+		fmt.Fprint(w, "验证通过!<br>")
+		fmt.Fprintf(w, "title 的值为: %v <br>", title)
+		fmt.Fprintf(w, "title 的长度为: %v <br>", len(title))
+		fmt.Fprintf(w, "body 的值为: %v <br>", body)
+		fmt.Fprintf(w, "body 的长度为: %v <br>", len(body))
+	} else {
+		html := `
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+ 	   	<title>创建文章 —— 我的技术博客</title>
+    	<style type="text/css">.error {color: red;}</style>
+		</head>
+		<body>
+    <form action="{{ .URL }}" method="post">
+        <p><input type="text" name="title" value="{{ .Title }}"></p>
+        {{ with .Errors.title }}
+        <p class="error">{{ . }}</p>
+        {{ end }}
+        <p><textarea name="body" cols="30" rows="10">{{ .Body }}</textarea></p>
+        {{ with .Errors.body }}
+        <p class="error">{{ . }}</p>
+        {{ end }}
+        <p><button type="submit">提交</button></p>
+    </form>
+		</body>
+		</html>`
+		storeURL, _ := router.Get("articles.store").URL()
+		data := ArticlesFormData{
+			Title:  title,
+			Body:   body,
+			URL:    storeURL,
+			Errors: errors,
+		}
+		tmpl, err := template.New("create-form").Parse(html)
+		if err != nil {
+			panic(err)
+		}
+
+		err = tmpl.Execute(w, data)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
 func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>创建文章</h1>")
+	html := `
+	<!DOCTYPE html>
+		<html lang="en">
+		<head>
+    	<title>创建文章 —— 我的技术博客</title>
+		</head>
+		<body>
+    	<form action="%s" method="post">
+      	<p><input type="text" name="title"></p>
+      	<p><textarea name="body" cols="30" rows="10"></textarea></p>
+      	<p><button type="submit">提交</button></p>
+    	</form>
+		</body>
+	</html>`
+	storeURL, _ := router.Get("articles.store").URL()
+	fmt.Fprintf(w, html, storeURL)
 }
 
 func forceHTMLMiddleware(next http.Handler) http.Handler {
@@ -48,20 +137,16 @@ func removeTrilingSlash(next http.Handler) http.Handler {
 	})
 }
 func main() {
-	router := mux.NewRouter()
 	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
 	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
 
 	router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
 	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
-	router.HandleFunc("/articles", articlesCreateHandler).Methods("POST").Name("articles.create")
+	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
+	router.HandleFunc("/articles/create", articlesCreateHandler).Methods("GET").Name("articles.create")
 
 	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
 	router.Use(forceHTMLMiddleware)
 
-	homeUrl, _ := router.Get("home").URL()
-	fmt.Println(homeUrl)
-	articleURL, _ := router.Get("articles.show").URL("id", "23")
-	fmt.Println("articleURL: ", articleURL)
 	http.ListenAndServe(":3000", removeTrilingSlash(router))
 }
