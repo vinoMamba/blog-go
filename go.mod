b21d20ea6eb84339f749af98751bbc6f7005310f
vino <vino0908@outlook.com>
2022-10-12 10:32:46 +0800

    add mux

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/vinoMamba/goblog
 
 go 1.18
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -3,16 +3,53 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
 }
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "About Page")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>关于我们</h1>")
 }
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>404 页面未找到</h1>")
+}
+func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	vars := mux.Vars(r)
+	fmt.Printf("vars: %#v", vars)
+	id := vars["id"]
+	fmt.Fprint(w, "<h1>文章</h1>")
+	fmt.Fprint(w, id)
+}
+func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>文章列表</h1>")
+}
+func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>创建文章</h1>")
+}
+
 func main() {
-	http.HandleFunc("/", defaultHandler)
-	http.HandleFunc("/about", aboutHandler)
-	http.ListenAndServe(":3000", nil)
+	router := mux.NewRouter()
+	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
+	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
+
+	router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
+	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
+	router.HandleFunc("/articles", articlesCreateHandler).Methods("POST").Name("articles.create")
+
+	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+
+	homeUrl, _ := router.Get("home").URL()
+	fmt.Println(homeUrl)
+	articleURL, _ := router.Get("articles.show").URL("id", "23")
+	fmt.Println("articleURL: ", articleURL)
+	http.ListenAndServe(":3000", router)
 }
